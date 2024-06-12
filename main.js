6067e923362fc29da0b42a73c503a709d8259d8d
Hacker94 <83153199+Ketan-vscode@users.noreply.github.com>
2024-06-12 16:39:21 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,45 @@
+img = "";
+noseX = 0;
+noseY = 0;
+marioX = 325;
+marioY = 325;
+
+
+function preload() {
+	world_start = loadSound("world_start.wav");
+	mario_jump = loadSound("jump.wav");
+	mario_coin = loadSound("coin.wav");
+	mario_kick = loadSound("kick.wav");
+	mario_die = loadSound("mariodie.wav");
+	world_start = loadSound("world_start.wav");
+	setSprites();
+	MarioAnimation();
+}
+
+function setup() {
+	canvas = createCanvas(1240, 336);
+	instializeInSetup(mario);
+
+	createCanvas(650, 400);
+	video = createCapture(VIDEO);
+	video.size(600, 300);
+
+	poseNet = ml5.poseNet(video, modelLoaded);
+	poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+	console.log('Model Loaded!');
+}
+
+function gotPoses(results) {
+	if (results.length > 0) {
+		noseX = results[0].pose.nose.x;
+		noseY = results[0].pose.nose.y;
+		console.log("noseX = " + noseX + ", noseY = " + noseY);
+	}
+}
+
+function draw() {
+	game();
+}
